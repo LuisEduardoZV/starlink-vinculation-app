ee36aa94752973d4dd3d667b8ec36057de4f5c6f
Luis Z <lezv.dc@gmail.com>
2024-02-07 10:33:12 -0600

    Cambios

--- src/config.js
+++ src/config.js
@@ -1,16 +1,6 @@
 export const BASE_PATH = ''
 
-// export const BASE_URL = 'https://test-tangraph.ever-track.com'
-export const BASE_URL = 'https://ws-tangraph.ever-track.com'
-
-// export const BASE_URL_API = 'https://test-tangraph.ever-track.com/api'
-export const BASE_URL_API = 'https://ws-tangraph.ever-track.com/api'
-
-export const BASE_URL_GRAFANA = 'http://starlink.tangerinemetrics.com/api/'
-
-export const GRAFANA_TOKEN = 'YWRtaW46YmxhY2thbnNpYmxl'
-export const GRAFANA_USER = 'admin'
-export const GRAFANA_PASSWORD = 'blackansible'
+export const BASE_URL_API = 'https://test-tangraph.ever-track.com/api'
 
 export const DASHBOARD_PATH = '/clients'
 export const DASHBOARD_PATH_NOADMIN = '/terminals'
